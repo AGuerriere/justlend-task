84c3cc223ecb5895266f0fc70eec21e3912b1e56
Antonio Guerriere <antonioguerriere@gmail.com>
2022-10-11 10:06:21 +0100

    wip

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,12 +17,12 @@ function LoginForm({ Login, error }) {
         {(error != "")? (<div className="error">{error}</div>) : ""}
 
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+          <label htmlFor="email">Email address</label>
+          <input type="email" name="email" id="email" placeholder="Enter email address" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="Enter your password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
         </div>
         <div className="form-group">
         <a href="#" className="forgot-password">Forgot password?</a>
